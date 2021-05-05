7ceedc1ac820c9533b33b58e5fad8ec04316c645
George Flores <georgeflores886@gmail.com>
2021-05-05 13:52:09 -0400

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+const Navbar = () => {
+    return (
+        <div>
+            <Button aria-controls='simple-menu' aria-haspopup='true'>Open Menu</Button>
+        </div>
+    )
+}
+
+export default Navbar
