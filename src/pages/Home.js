c0b32f55f3824886cce96d69aa6df6822a787e6a
George Flores <georgeflores886@gmail.com>
2021-05-06 13:42:07 -0400

    adjusted critics styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import Divider from '@material-ui/core/Divider'
-import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
 import Paper from '@material-ui/core/Paper'
 
 import backgroundImage1 from '../images/backgroundimage-1.gif'
@@ -12,6 +13,12 @@ import '../styles/Home.css'
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
+    },
+    gridListStyle: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden'
     },
     backgroundStyle: {
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)), url(${backgroundImage1})`,
@@ -22,8 +29,13 @@ const useStyles = makeStyles(theme => ({
     },
     paper: {
         padding: theme.spacing(2),
+        paddingBottom: '1.2rem',
         textAlign: 'center',
-        backgroundColor: `linear-gradient(rgb(253,250,75), rgb(54,53,50))`
+        justifyContent: 'center',
+        height: '100%',
+        opacity: 0.8,
+        background: '#000',
+        color: '#fff'
     }
 }))
 
@@ -32,24 +44,28 @@ const Home = () => {
     const classes = useStyles()
 
     return (
-        <Container max-width='xl' className='home-container'>
+        <Container max-width='false' className='home-container'>
             <div className='hero-banner'></div>
             <Divider />
             <div className={classes.backgroundStyle}>
                 <div className={classes.root}>
-                    <Grid container spacing={3}>
-                        <Grid item xs={4}>
-                            <Paper className={classes.paper}><em>"You can now enjoy the masterpiece in its best form"</em> - PCMag</Paper>
+                    <Grid container spacing={3} alignContent='center' justifyContent='center'>
+                        <Grid item md={4}>
+                            <Paper className={classes.paper}><em>"You can now enjoy the masterpiece in its best form"</em> <br /> <p> - PCMag</p></Paper>
                         </Grid>
-                        <Grid item xs={4}>
-                            <Paper className={classes.paper}><em>"A massive JRPG that looks better than ever on PC"</em> - Game Rant</Paper>
+                        <Grid item md={4}>
+                            <Paper className={classes.paper}><em>"A massive JRPG that looks better than ever on PC"</em> <br /> <p>- Game Rant </p></Paper>
                         </Grid>
-                        <Grid item xs={4}>
-                            <Paper className={classes.paper}><em>"...A capivating JRPG, marrying slice-of-life drama with stylish demon battling"</em> - PC Gamer</Paper>
+                        <Grid item md={4}>
+                            <Paper className={classes.paper}><em>"...A capivating JRPG, marrying slice-of-life drama with stylish demon battling"</em><br /><p>- PC Gamer</p> </Paper>
                         </Grid>
                     </Grid>
                 </div>
             </div>
+
+            <section className={classes.gridListStyle}>
+                <h2>Media</h2>
+            </section>
         </Container>
     )
 }
