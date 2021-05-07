17d25c1fab98e30ca87834204fcdcbeb1087d5e3
George Flores <georgeflores886@gmail.com>
2021-05-07 13:00:42 -0400

    fixed formatting for critics section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,7 +19,7 @@ const useStyles = makeStyles(theme => ({
         flexWrap: 'wrap',
         justifyContent: 'space-around',
         overflow: 'hidden',
-        backgroundColor: theme.palette.background.paper
+        backgroundColor: '#FFFB41',
     },
     gridList: {
         width: '100%',
@@ -28,21 +28,21 @@ const useStyles = makeStyles(theme => ({
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)), url(${backgroundImage1})`,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
         width: '100%',
         padding: '10rem'
     },
     paper: {
         padding: theme.spacing(2),
-        paddingBottom: '1.2rem',
+        width: 200,
+        height: 150,
+        lineHeight: 1.5,
         textAlign: 'center',
-        justifyContent: 'center',
-        height: '100%',
         opacity: 0.7,
         background: '#000',
         color: '#fff'
     }
 }))
-
 
 const Home = () => {
     const classes = useStyles()
@@ -53,7 +53,7 @@ const Home = () => {
             <Divider />
             <div className={classes.backgroundStyle}>
                 <div className={classes.root}>
-                    <Grid container spacing={3} alignContent='center' justifyContent='center'>
+                    <Grid container spacing={2} alignContent='center' justify='center'>
                         <Grid item md={4}>
                             <Paper className={classes.paper}><em>"You can now enjoy the masterpiece in its best form"</em> <br /> <p> - PCMag</p></Paper>
                         </Grid>
@@ -67,11 +67,11 @@ const Home = () => {
                 </div>
             </div>
 
-            <h1 className='media-title'>Media</h1>
             <section className={classes.gridListRoot}>
+                <h1 className='media-title'>Media</h1>
                 <GridList cellHeight='auto' className={classes.gridList} cols={4}>
                     {screenshotData.map(tile => (
-                        <GridListTile key={tile.img} cols={tile.cols || 1}>
+                        <GridListTile key={tile.img}>
                             <img className='screenshots' src={tile.img} alt={tile.alt} />
                         </GridListTile>
                     ))}
