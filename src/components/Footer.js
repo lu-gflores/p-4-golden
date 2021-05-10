b4d22ddd86ebb64f4435ac76dcf080e1f767d56e
George Flores <georgeflores886@gmail.com>
2021-05-10 10:47:03 -0400

    updated footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,36 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Container from '@material-ui/core/Container'
 import Toolbar from '@material-ui/core/Toolbar'
-import Grid from '@material-ui/core/Grid'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
 
+import Rating from '../images/logos/rating.jpg'
 import Atlus from '../images/logos/Atlus_Logo.png'
 import Steam from '../images/logos/steam_logo_rev.png'
-import PSVita from '../images/logos/vita.png'
+import PSVita from '../images/logos/psvita.png'
+import '../styles/Footer.css'
+const useStyles = makeStyles(theme => ({
+    footer: {
+        background: 'linear-gradient(#FFFB41, #000)',
+        display: 'flex',
+        width: '100%',
+        padding: '20px 0',
+        alignContent: 'center'
+    }
+}))
 const Footer = () => {
+    const classes = useStyles()
     return (
-        <AppBar position='static' className='main-footer'>
+        <AppBar position='static' className={classes.footer}>
             <Container maxWidth='md'>
                 <Toolbar>
-                    <img src={Atlus} alt='Atlus Co.' />
-                    <img src={Steam} alt='Steam' />
-                    <img src={PSVita} alt='PS Vita' />
-                    &copy; 2021 lu-gflores
-            </Toolbar>
+                    <img className='logo' src={Rating} alt='ESRB Mature Rating' />
+                    <img className='logo' src={Atlus} alt='Atlus Co.' />
+                    <img className='logo' src={Steam} alt='Steam' />
+
+                </Toolbar>
+                <Toolbar>
+                    Site Created by Copyright&copy; 2021 lu-gflores
+                </Toolbar>
             </Container>
         </AppBar>
     )
