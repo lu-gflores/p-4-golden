f46c1da01c36e638e866df8856ea6a9f0422d051
George Flores <georgeflores886@gmail.com>
2021-05-10 11:54:16 -0400

    created styles for story title and background

--- src/pages/Story.js
+++ src/pages/Story.js
@@ -5,24 +5,45 @@ import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
 
 import StoryBackground from '../images/story-background-image.jpg'
+import heroBanner from '../images/story-images/story-title.gif'
 
 const useStyles = makeStyles(() => ({
-    coverImage: {
+    backgroundImage: {
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)), url(${StoryBackground})`,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         width: '100%',
         height: '100vh'
+    },
+    storyTitle: {
+        textAlign: 'center',
+        padding: '2.5rem',
+        fontSize: '6em',
+        textDecoration: 'underline',
+        opacity: 0.5
+    },
+    heroBanner: {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)), url(${heroBanner})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        borderRadius: '10% 10% / 10% 40%',
+        width: '100%',
+        height: '60vh'
     }
 }))
 
 const Story = () => {
     const classes = useStyles()
     return (
-        <Container className={classes.coverImage}>
+        <Container className={classes.backgroundImage}>
+            <div className={classes.heroBanner}>
+                <h1 className={classes.storyTitle}>Story</h1>
+            </div>
             <Box>
-                <h1>Story</h1>
+
             </Box>
+
         </Container>
     )
 }
