4d8a085a2d18883bf3c65cf67fde46050707ba59
George Flores <georgeflores886@gmail.com>
2021-05-11 10:48:10 -0400

    added box content and styles for text

--- src/pages/Story.js
+++ src/pages/Story.js
@@ -3,9 +3,11 @@ import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
-
+import Paper from '@material-ui/core/Paper'
+// Images
 import StoryBackground from '../images/story-background-image.jpg'
 import heroBanner from '../images/story-images/story-title.gif'
+import MidnightChannel from '../images/story-images/MidnightChannel-Intro.jpg'
 
 const useStyles = makeStyles(() => ({
     backgroundImage: {
@@ -30,6 +32,33 @@ const useStyles = makeStyles(() => ({
         borderRadius: '10% 10% / 10% 40%',
         width: '100%',
         height: '60vh'
+    },
+    paperIntro: {
+        background: `linear-gradient(0deg, rgba(156,203,227,1) 9%, rgba(255,255,255,1) 100%)`,
+        border: '5px solid rgb(156,203,227)',
+        padding: '1.5rem',
+        marginBottom: '1rem',
+        borderRadius: '30px',
+        transform: 'rotate(3deg)',
+        color: '#004f77',
+        zIndex: 9999
+    },
+    paperSection1: {
+        background: `linear-gradient(0deg, rgba(156,203,227,1) 9%, rgba(255,255,255,1) 100%)`,
+        border: '5px solid rgba(235, 40, 126, 100)',
+        padding: '1.5rem',
+        marginTop: '1.2rem',
+        marginBottom: '1rem',
+        borderRadius: '30px',
+        transform: 'rotate(-3deg)',
+        color: '#004f77',
+    },
+    introImage: {
+        width: 400,
+        height: 400,
+        borderRadius: '35px',
+        alignContent: 'center',
+        opacity: 0.8
     }
 }))
 
@@ -41,9 +70,30 @@ const Story = () => {
                 <h1 className={classes.storyTitle}>Story</h1>
             </div>
             <Box>
-
+                <Paper className={classes.paperIntro}>
+                    <p>
+                        A rumor around school says that by watching a TV screen at midnight on a rainy night, the face of your soulmate will be revealed.
+                        However, a chain of murders seems to be connected to the rumor. Therefore, you must gather up a team to brave the strange world
+                        inside the TV to solve this mystery.
+                    </p>
+                </Paper>
             </Box>
 
+            <Box>
+                <Grid container spacing={2}>
+                    <Grid item md={6} xs={12}>
+                        <Paper className={classes.paperSection1}>
+                            <p>
+                                This is a coming of age story that sets the protagonist and his friends on a journey kickstarted by a chain of serial murders. You
+                                will meet kindred spirits, feel a sense of belonging, and confront the darker sides of one's self.
+                        </p>
+                        </Paper>
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+
+                    </Grid>
+                </Grid>
+            </Box>
         </Container>
     )
 }
